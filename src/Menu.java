fed3f4a8aa16bf82f86e3ba950f8d903ee1fc335
Tenurian <tfeuerbo@gmail.com>
2015-07-06 09:43:13 -0600

    Updated my code, added some simplicity.
    Signed-off-by: Tenurian <tfeuerbo@gmail.com>

--- src/Menu.java
+++ src/Menu.java
@@ -7,97 +7,125 @@ public class Menu {
 	private UserInterface ui = new UserInterface();
 	private PlayerVsPlayer pvp = new PlayerVsPlayer();
 	private Scanner scan = new Scanner(System.in);
-	
+
 	public void mainMenu(){
-		
-		
+
+
 		boolean menuLoop = true;
-		
+
 		try{
-		while(menuLoop){
-			ui.displayMain();
-			int userIn = Integer.parseInt(scan.nextLine());
-			
-			if (userIn == 1){
-				pvp.pvpGame();
-				menuLoop = playAgain();
+			while(menuLoop){
+				ui.displayMain();
+				int userIn = Integer.parseInt(scan.nextLine());
+
+				if (userIn == 1){
+					pvp.pvpGame();
+					menuLoop = playAgain();
+				}
+
+				else if (userIn == 2){
+
+					//Runs compuer vs player
+					menuLoop = playAgain();
+				}
+
+				else if (userIn == 3){
+
+					//Runs computer vs computer
+					menuLoop = playAgain();
+
+				}
+
+				else if (userIn == 4){
+
+					//Exit
+					System.out.println("Goodbye!");
+					System.exit(1);
+
+				}
+
+
+				else {
+
+					System.out.println("That is not a valid number, please imput a valid number...\n");
+
+
+				}
+
+/************************Use for Refactoring***************************/
+//				switch(userIn){
+//				case 1:
+//					pvp.pvpGame();
+//					menuLoop = playAgain();
+//					break;
+//				case 2:
+//					menuLoop = playAgain();
+//					break;
+//				case 3:
+//					menuLoop = playAgain();
+//					break;
+//				case 4:
+//					System.out.println("Goodbye!");
+//					System.exit(1);
+//					break;
+//				default:
+//					break;
+//				}
+
 			}
-			
-			else if (userIn == 2){
-				
-				//Runs compuer vs player
-				menuLoop = playAgain();
-			}
-			
-			else if (userIn == 3){
-				
-				//Runs computer vs computer
-				menuLoop = playAgain();
-				
-			}
-			
-			
-			else {
-				
-				System.out.println("That is not a valid number, please imput a valid number...\n");
-				
-				
-			}
-		}
 		}
 		catch(NumberFormatException e){
-			
+
 			System.out.println("That is not a number, please try a number..");
 			mainMenu();
-			
+
 		}
 	}
-	
+
 	public boolean playAgain(){
-		
+
 		ui.promptToPlayAgain();
 		boolean playAgainLoop = true;
 		boolean promptAwnser = false;
-		
+
 		try{
-		while(playAgainLoop){
-			int response = Integer.parseInt(scan.nextLine());
-		
-			if (response == 1){
-				
-				promptAwnser = true;
-				playAgainLoop = false;
-				
+			while(playAgainLoop){
+				int response = Integer.parseInt(scan.nextLine());
+
+				if (response == 1){
+
+					promptAwnser = true;
+					playAgainLoop = false;
+
+				}
+
+				else if (response == 2){
+
+					promptAwnser = false;
+					playAgainLoop = false;
+				}
+
+				else {
+
+					System.out.println("That is not a valid number, please imput a valid number...\n");
+					ui.promptToPlayAgain();
+
+				}
+
 			}
-			
-			else if (response == 2){
-				
-				promptAwnser = false;
-				playAgainLoop = false;
-			}
-			
-			else {
-				
-				System.out.println("That is not a valid number, please imput a valid number...\n");
-				ui.promptToPlayAgain();
-				
-			}
-			
-		}
 		}
 		catch(NumberFormatException e){
-			
+
 			System.out.println("You need to imput a value...");
 			playAgain();
-			
+
 		}
-		
+
 		return promptAwnser;
-		
-}
-	
+
+	}
+
 }
 
-	
-	//Dont forget to put in user imputs as well
-	
+
+//Dont forget to put in user imputs as well
